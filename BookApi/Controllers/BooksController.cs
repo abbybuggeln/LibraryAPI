c16e638e7a7d616b3a543b15018b7827206f6e7b
abbybuggeln <abigailbuggeln@gmail.com>
2024-07-28 19:06:49 -0700

    Uploading API to github, SQL integration next

---
+++ BookApi/Controllers/BooksController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using BookApi.Data;
+using BookApi.Models;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookStore _bookStore;
+
+        public BooksController()
+        {
+            _bookStore = new BookStore();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return _bookStore.GetBooks();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = _bookStore.GetBookById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> AddBook(Book book)
+        {
+            _bookStore.AddBook(book);
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingBook = _bookStore.GetBookById(id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            _bookStore.UpdateBook(book);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = _bookStore.GetBookById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookStore.DeleteBook(id);
+            return NoContent();
+        }
+    }
+}
---
+++ BookApi/Data/BookStore.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookApi.Models;
+
+namespace BookApi.Data
+{
+    public class BookStore
+    {
+        private static List<Book> Books = new List<Book>
+        {
+            new Book { Id = 1, Title = "1984", Author = "George Orwell", Year = 1949 },
+            new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960 }
+        };
+
+        public List<Book> GetBooks() => Books;
+
+        public Book GetBookById(int id) => Books.FirstOrDefault(b => b.Id == id);
+
+        public void AddBook(Book book)
+        {
+            book.Id = Books.Max(b => b.Id) + 1;
+            Books.Add(book);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            var index = Books.FindIndex(b => b.Id == book.Id);
+            if (index != -1)
+            {
+                Books[index] = book;
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            var book = Books.FirstOrDefault(b => b.Id == id);
+            if (book != null)
+            {
+                Books.Remove(book);
+            }
+        }
+    }
+}
